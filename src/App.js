639a40f5f8a39bd5deba647ff764146567e9b9ea
Barış Emre Yalçın <barisemre.bey@gmail.com>
2024-08-08 18:28:05 +0300

    complete video player

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Video from "./components/Video";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null); // Json file'dan çekilecek veri
+  const [isLoading, setIsLoading] = useState(true); // Veri yüklenme durumu
+  const [error, setError] = useState(null); // Veri çekerken hata alma durumu
+
+  // Data fetch
+  const fetchData = async function () {
+    try {
+      const res = await fetch("assets/Publishment.json");
+      if (!res.ok) throw new Error("Data alınırken hata!");
+      const data = await res.json();
+      setData(data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // Sayfa yüklenirken data fetch et
+  useEffect(function () {
+    fetchData();
+  }, []);
+
+  // Scene styles
+  const sceneStyles = data
+    ? {
+        width: data.style.width,
+        height: data.style.height,
+      }
+    : {};
+
+  if (isLoading) return <div className="loading">LOADING...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={sceneStyles}>
+      {data.components.map((component) => (
+        <Video key={component.id} component={component} />
+      ))}
     </div>
   );
 }
---
+++ src/components/Video.js
@@ -0,0 +1,58 @@
+import { useEffect, useRef, useState } from "react";
+
+function Video({ component }) {
+  const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (!video) return;
+
+    video.src = component.file;
+    video.loop = component.info.repeat;
+    if (isPlaying) {
+      video.play();
+    }
+  }, [component, isPlaying]);
+
+  const handlePlay = () => {
+    setIsPlaying(true);
+  };
+
+  const videoStyles = {
+    position: component.style["position"],
+    width: component.style["width"],
+    height: component.style["height"],
+    top: component.style["top"],
+    left: component.style["left"],
+    borderRadius: component.style["border-radius"],
+    backgroundColor: component.style["background-color"],
+    backgroundPosition: component.style["background-position"],
+    backgroundRepeat: component.style["background-repeat"],
+    backgroundSize: component.style["background-size"],
+    fontFamily: component.style["font-family"],
+    opacity: component.style["opacity"],
+    filter: component.style["filter"],
+    lineHeight: component.style["line-height"],
+    boxShadow: component.style["box-shadow"],
+    zIndex: component.style["z-index"],
+    objectFit: component.style["object-fit"],
+    right: component.style["right"],
+    bottom: component.style["bottom"],
+  };
+
+  return (
+    <div>
+      <video
+        ref={videoRef}
+        style={videoStyles}
+        controls
+        preload="auto"
+        autoPlay={isPlaying}
+      />
+      {!isPlaying && <button onClick={handlePlay}>Play Video</button>}
+    </div>
+  );
+}
+
+export default Video;
